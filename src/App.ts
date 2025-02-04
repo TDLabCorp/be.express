1fe614c8731a08e720eb24bece8f8b30c5d1c1bb
김태용 <taey.kim@lge.com>
2025-02-04 16:04:36 +0900

    - 시작점을 index.js -> App.js로 변경 - Logger 함수 추가

--- src/App.ts
+++ src/App.ts
@@ -1,12 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import { logger } from "./Logger";
 
 const app: Express = express();
 const port = 5000;
 
 app.get("/", (request: Request, response: Response) => {
+  logger(`Typescript + Node.js + Express Server`);
   response.send(`Typescript + Node.js + Express Server`);
 });
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at <https://localhost>:${port}`);
+  logger(`[server]: Server is running at <https://localhost>:${port}`);
 });
