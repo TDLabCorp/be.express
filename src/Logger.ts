c44c5bb20960740d04ea6739df6a5b92b066f800
김태용 <taey.kim@lge.com>
2025-02-07 14:42:52 +0900

    - winston 설치 및 셋팅 - 로컬 debug 메시지만 처리

--- src/Logger.ts
+++ src/Logger.ts
@@ -1,3 +1,33 @@
+import { createLogger, format, transports } from "winston";
+import { TransformableInfo } from "logform";
+
+const _logger = createLogger({
+  transports: [
+    new transports.Console({
+      level: "debug",
+      format: format.combine(
+        format.label({ label: "[localhost]" }),
+        format.timestamp({
+          format: "YYYY-MM-DD HH:mm:ss",
+        }),
+        format.colorize(),
+        format.printf(
+          (info: TransformableInfo) =>
+            `${info.timestamp} - ${info.level}: ${info.label} ${info.message}`
+        )
+      ),
+    }),
+  ],
+});
+
 export const logger = (message: string) => {
-  console.log(message);
+  _logger.debug(message);
 };
+
+// export function logger(message: string) {
+//   _logger.debug(message);
+
+//   // return {
+//   //   debug: (message: string) => {},
+//   // };
+// }
